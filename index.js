4aafbd2d43ff1d0ad2fef209fed47f6db0955059
Yonus Bakhshi <yonus.bakhshi@gmail.com>
2024-02-28 12:36:13 +0100

    payment and client

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
+const bodyParser = require('body-parser')
+
 require('./models/Users');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
-
+app.use(bodyParser.json())
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -21,6 +23,18 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
+if(process.env.NODE_NEW === 'production'){
+  // express serves up prod assets like main.js and main.css
+  app.use(express.static('client/build'))
+
+  // express servs up the index.html if it does not recognize the route
+  const path  = require('path')
+  app.get('*', (req, res) =>{
+      res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,11 +8,17 @@ module.exports = app => {
     })
   );
 
-  app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get(
+        '/auth/google/callback', 
+        passport.authenticate('google'),
+        (req, res) =>{
+          res.redirect('/surveys')
+        }
+    );
 
   app.get('/api/logout', (req, res) => {
     req.logout() // attach by password to the req
-    res.send(req.user)
+    res.redirect('/')
   })
 
   app.get('/api/current_user', (req, res) => {
